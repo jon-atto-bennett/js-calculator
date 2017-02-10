cb7d6a6e472e584d1cec9469fe886232baf2d295
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-02-10 16:49:31 +1300

    js refactor

--- script.js
+++ script.js
@@ -6,22 +6,19 @@ function submitNum(num) {
   if(newNum) {
     input = "";
   }
-input = input + num;
-document.myCalculator.display.value=input;
-newNum=false;
+  input = input + num;
+  document.myCalculator.display.value=input;
+  newNum=false;
 };
 
 function submitOperator(op) {
-
-    sum.push(input);
-    sum.push(op);
-    newNum = true;
-    getResult(sum);
-
-    }
+  sum.push(input);
+  sum.push(op);
+  newNum = true;
+  getResult(sum);
+};
 
 function getResult(){
-
   var total =sum[0];
   var op = "";
   var nextNum = 0;
@@ -29,32 +26,25 @@ function getResult(){
   for(i=1; i<sum.length-1; i=i+2){
     op = sum[i];
     nextNum = sum[i+1];
-    if(op==="+"){
+    if(op ==="+"){
       total = parseFloat(total) + parseFloat(nextNum);
-      document.myCalculator.display.value=total;
     }
-    else if (op==="-"){
+    else if (op ==="-"){
       total = parseFloat(total) - parseFloat(nextNum);
-      console.log(total);
+    }
+    else if (op ==="*"){
+      total = parseFloat(total) * parseFloat(nextNum);
+    }
+    else if (op ==="/"){
+      total = parseFloat(total) / parseFloat(nextNum);
+    }
   }
-  else if (op==="*"){
-    total = parseFloat(total) * parseFloat(nextNum);
-    console.log(total);
-}
-  else if (op==="/"){
-  total = parseFloat(total) / parseFloat(nextNum);
-  console.log(total);
-}
-
-}
-
   document.myCalculator.display.value=total;
-}
+};
 
 
 function clearDisplay(){
 input = "";
 sum =[];
-equate=false;
 document.myCalculator.display.value = input;
-}
+};
